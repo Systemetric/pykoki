0a136412b84fc39e128fd25e1e88dfd889ddfde1
Josh Holland <anowlcalledjosh@gmail.com>
2017-10-29 22:55:37 +0000

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup( name = "pykoki", 
-       version = "0.0.1",
-       install_requires = ["v4l2",],
-       packages = ["pykoki"],
+setup(
+    name = "pykoki",
+    version = "0.0.1",
+    install_requires = ["v4l2"],
+    packages = ["pykoki"],
 
-       author = "Chris Kirkham",
-       author_email = "chrisjameskirkham@gmail.com",
-       url="https://www.studentrobotics.org/trac/wiki/libkoki",
+    author = "Chris Kirkham",
+    author_email = "chrisjameskirkham@gmail.com",
+    url="https://www.studentrobotics.org/trac/wiki/libkoki",
 )
